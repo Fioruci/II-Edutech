d36fffd343d9fb5e133ac475ba503291420ff9a4
Fioruci <49049830+Fioruci@users.noreply.github.com>
2025-08-19 20:24:14 -0300

    Create Exercicio2.java

---
+++ Exercicio2.java
@@ -0,0 +1,27 @@
+// Alexandre, Bruno Aoki, Bruno David, Fioruci
+public class Codigo2 {
+    public static void main(String[] args) {
+        // Produtos e seus respectivos preços: Xuxu: 4.99 ... Maçã: 3.99
+        String[] produtos = {"Xuxu", "Melão", "Goiaba", "Maçã"};
+        double[] precos = {4.99, 2.99, 1.99, 3.99};
+        
+        // BubbleSort
+        for (int x = 0; x < produtos.length - 1; x++) {
+            for (int y = x + 1; y < produtos.length; y++) {
+                if (produtos[x].compareTo(produtos[y]) > 0) {
+                    String aux = produtos[x];
+                    produtos[x] = produtos[y];
+                    produtos[y] = aux;
+                    
+                    double aux2 = precos[x];
+                    precos[x] = precos[y];
+                    precos[y] = aux2;
+                }
+            }
+        }
+        
+        for (int cont = 0; cont < produtos.length; cont++) {
+            System.out.println(produtos[cont] + ": " + precos[cont]);
+        } 
+    } 
+}
