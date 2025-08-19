f7c76857c6364f6223320e8c8e26ae848f04a171
Fioruci <49049830+Fioruci@users.noreply.github.com>
2025-08-19 20:27:04 -0300

    fix: sou burro

--- Exercicio3.js
+++ Exercicio3.js
@@ -3,7 +3,7 @@ async function buscarDados() {
     const request = await fetch("https://jsonplaceholder.typicode.com/posts/1");
     const data = await request.json();
     
-    return dta;
+    return data;
 }
 
 async function main() {
